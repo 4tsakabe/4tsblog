20fcdcaa71f7a1f732be4375c9fea00264f395fa
yotsutose <becchi0424@gmail.com>
2023-03-08 21:02:03 +0900

    fix apos

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ export const Header: FC = () => {
         <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
           <NextLink href="/" passHref>
             <Heading as='h1' fontSize="2xl" cursor="pointer">
-              よつメモ / 4ts's Blog
+              よつメモ / 4ts&apos;s Blog
             </Heading>
           </NextLink>
         </Flex>
