94b51647610ae516a00103939ee86a735ca5241e
yotsutose <becchi0424@gmail.com>
2023-03-08 21:01:06 +0900

    Headerの作成

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import {
+  Box,
+  Flex,
+  Container,
+  Heading,
+} from '@chakra-ui/react';
+import NextLink from "next/link";
+import { FC } from 'react';
+
+export const Header: FC = () => {    
+  return (
+    <Box px={4} bgColor="gray.100">
+      <Container maxW="container.lg">
+        <Flex as="header" py="4" justifyContent="space-between" alignItems="center">
+          <NextLink href="/" passHref>
+            <Heading as='h1' fontSize="2xl" cursor="pointer">
+              よつメモ / 4ts's Blog
+            </Heading>
+          </NextLink>
+        </Flex>
+      </Container>
+    </Box>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
-/* src/pages/index.tsx */
 import type { NextPage } from 'next'
-import { Heading, Container } from '@chakra-ui/react'
+import { Header } from '../components/Header'
 
 const Home: NextPage = () => {
   return (
-    <Container>
-      <Heading as="h1" fontSize="3xl" mt="10">
-        こんにちは、Next.js
-      </Heading>
-    </Container>
+    <Header />
   )
 }
+
 export default Home
